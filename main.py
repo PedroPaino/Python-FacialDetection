6e0f38752593b5da2c2e66f76631f78a3a45d80f
PedroPaino <pedropainoadm@gmail.com>
2025-05-09 01:53:04 -0300

    Primeiro commit do projeto de detecção facial

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-import cv2 
+import cv2
 from cvzone.FaceDetectionModule import FaceDetector
 
+# Inicializa a captura de vídeo e o detector de faces
 video = cv2.VideoCapture(0)
-
-detector = FaceDetector
+detector = FaceDetector()
 
 while True:
-    _,img = video.read()
-    img.bboxes = detector.findFaces(img,draw=True)
-
-    cv2.imshow ("Resultado'",img)
-    if cv2.waitKey(1) ==27:
+    # Lê o frame da câmera
+    ret, img = video.read()
+    if not ret:
+        print("Erro ao acessar a câmera.")
         break
+
+    # Detecta rostos no frame
+    img, bboxes = detector.findFaces(img, draw=True)
+
+    # Exibe o resultado
+    cv2.imshow("Resultado", img)
+
+    # Sai do loop ao pressionar 'Esc'
+    if cv2.waitKey(1) == 27:
+        break
+
+# Libera os recursos
+video.release()
+cv2.destroyAllWindows()
